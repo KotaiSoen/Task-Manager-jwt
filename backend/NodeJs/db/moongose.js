ef096462715108481b1c83cf35d3fae2cbc6d797
KotaiSoen <92819012+KotaiSoen@users.noreply.github.com>
2022-04-27 11:13:47 +0100

    Update moongose.js

--- backend/NodeJs/db/moongose.js
+++ backend/NodeJs/db/moongose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoURI = 'mongodb+srv://Soen:17^Th082000@nodejs.7dcz4.mongodb.net/nodejs?retryWrites=true&w=majority'
+mongoURI = 'mongodb+srv://password@nodejs.7dcz4.mongodb.net/project-name?retryWrites=true&w=majority'
 
 mongoose.connect(mongoURI, { useNewUrlParser: true})
     .then(() => {
